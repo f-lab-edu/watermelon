039ad10ef78eb319deae67e32fd1ff1342b60b6d
Jun Kim <82041804+Kim-HJ1986@users.noreply.github.com>
2024-05-11 11:08:17 +0900

    초기 환경 구성

--- src/main/java/com/project/watermelon/WatermelonApplication.java
+++ src/main/java/com/project/watermelon/WatermelonApplication.java
@@ -4,7 +4,9 @@ import com.project.watermelon.util.RedisUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class WatermelonApplication {
 
