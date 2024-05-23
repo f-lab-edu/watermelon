29d3141043544453da8f6674202fd217a2808bb4
Jun Kim <82041804+Kim-HJ1986@users.noreply.github.com>
2024-05-23 22:19:24 +0900

    memberController 수정
    members 복수형으로 지정 및 공통 path로 지정

--- main-server/src/main/java/com/project/watermelon/controller/MemberController.java
+++ main-server/src/main/java/com/project/watermelon/controller/MemberController.java
@@ -1,6 +1,5 @@
 package com.project.watermelon.controller;
 
-
 import com.project.watermelon.dto.member.MemberLoginRequestDto;
 import com.project.watermelon.dto.member.MemberSignUpRequestDto;
 import com.project.watermelon.dto.member.MemberSignUpResponseDto;
@@ -11,19 +10,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/members")
 public class MemberController {
 
     private final MemberService memberService;
 
-    @PostMapping("/member/signup")
+    @PostMapping("/signup")
     public ResponseEntity<MemberSignUpResponseDto> signup(@RequestBody MemberSignUpRequestDto memberRequestDto) {
         return ResponseEntity.ok(memberService.signup(memberRequestDto));
     }
 
-    @PostMapping("/member/login")
+    @PostMapping("/login")
     public ResponseEntity<TokenDto> login(@RequestBody MemberLoginRequestDto memberRequestDto) {
         return ResponseEntity.ok(memberService.login(memberRequestDto));
     }
