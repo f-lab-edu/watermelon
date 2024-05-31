0dfe6964a3ee1cb76630bfa7aa1f00b839b1c0a5
Jun Kim <82041804+Kim-HJ1986@users.noreply.github.com>
2024-05-31 22:42:21 +0900

    reservation update cronjob 중간 저장

--- consumer-server/src/main/java/com/project/consumerserver/repository/ReservationRepository.java
+++ consumer-server/src/main/java/com/project/consumerserver/repository/ReservationRepository.java
@@ -1,5 +1,6 @@
 package com.project.consumerserver.repository;
 
+import com.project.consumerserver.dto.ConcertMappingSeatInfoDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -34,6 +35,28 @@ public class ReservationRepository {
         return jdbcTemplate.query(sql, rowMapper);
     }
 
+    public List<ConcertMappingSeatInfoDTO> retrieveConcertMappingSeatCapacities() {
+        String sql = """
+        SELECT DISTINCT
+            C.CONCERT_MAPPING_ID,
+            L.SEAT_CAPACITY
+        FROM RESERVATION R
+        INNER JOIN CONCERT_MAPPING C
+        ON R.CONCERT_MAPPING_ID = C.CONCERT_MAPPING_ID
+        INNER JOIN LOCATION L
+        ON C.LOCATION_ID = L.LOCATION_ID
+        """;
+
+        RowMapper<ConcertMappingSeatInfoDTO> rowMapper = (resultSet, rowNum) -> {
+            ConcertMappingSeatInfoDTO info = new ConcertMappingSeatInfoDTO();
+            info.setConcertMappingId(resultSet.getLong("CONCERT_MAPPING_ID"));
+            info.setSeatCapacity(resultSet.getLong("SEAT_CAPACITY"));
+            return info;
+        };
+
+        return jdbcTemplate.query(sql, rowMapper);
+    }
+
     public Long retrieveConcertMappingSeatCapacity(Long concertMappingId) {
         String sql = """
         SELECT
@@ -46,5 +69,35 @@ public class ReservationRepository {
 
         RowMapper<Long> rowMapper = (resultSet, rowNum) -> resultSet.getLong("SEAT_CAPACITY");
         return jdbcTemplate.queryForObject(sql, rowMapper, concertMappingId);
+    }
+
+    public Long retrieveAvailableOrReservedCount(Long concertMappingId) {
+        String sql = """
+        SELECT
+            COUNT(*) AS COUNT
+        FROM RESERVATION
+        WHERE CONCERT_MAPPING_ID = ?
+        AND STATUS IN ('AVAILABLE', 'RESERVED')
+        """;
+
+        return jdbcTemplate.queryForObject(sql, Long.class, concertMappingId);
+    }
+
+    public void updateReservationStatus(Long concertMappingId, Long updateCount) {
+        String sql = """
+        UPDATE RESERVATION
+        SET STATUS = 'AVAILABLE'
+        WHERE RESERVATION_ID IN (
+            SELECT RESERVATION_ID
+            FROM (
+                SELECT RESERVATION_ID
+                FROM RESERVATION
+                WHERE CONCERT_MAPPING_ID = ? AND STATUS = 'WAIT'
+                ORDER BY RESERVATION_RANK ASC
+                LIMIT ?
+            ) AS subquery
+        )
+        """;
+        jdbcTemplate.update(sql, concertMappingId, updateCount);
     }
 }
--- consumer-server/src/main/java/com/project/consumerserver/service/ReservationCronJobService.java
+++ consumer-server/src/main/java/com/project/consumerserver/service/ReservationCronJobService.java
@@ -1,8 +1,11 @@
 package com.project.consumerserver.service;
 
+import com.project.consumerserver.dto.ConcertMappingSeatInfoDTO;
 import com.project.consumerserver.repository.ReservationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class ReservationCronJobService {
@@ -12,8 +15,18 @@ public class ReservationCronJobService {
     @Scheduled(cron = "0 */3 * * * *")
     public void updateReservation() {
         // 예메 테이블에서 유니크한 CONCERT_MAPPING_ID 조회
+        List<ConcertMappingSeatInfoDTO> concertMappingSeatInfoList = reservationRepository.retrieveConcertMappingSeatCapacities();
+
         // 해당 CONCERT_MAPPING_ID 별 예매 상태 업데이트 플로우 시작
         // 업데이트 가능한 수 = (LOCATION 의 SEAT_CAPACITY - CONCERT_MAPPING_ID 기준 AVAILABLE, RESERVED 상태인 로우 카운트)
+        for (ConcertMappingSeatInfoDTO concertMappingSeatInfo : concertMappingSeatInfoList) {
+            Long targetConcertMappingId = concertMappingSeatInfo.getConcertMappingId();
+            Long targetSeatCapacity = concertMappingSeatInfo.getSeatCapacity();
+            Long availableOrReservedCount = reservationRepository.retrieveAvailableOrReservedCount(targetConcertMappingId);
+            Long updateCount = (targetSeatCapacity - availableOrReservedCount);
+
+            reservationRepository.updateReservationStatus(targetConcertMappingId, updateCount);
+        }
         // 위와 같은 정책을 바탕으로 업데이트 진행
 
     }
