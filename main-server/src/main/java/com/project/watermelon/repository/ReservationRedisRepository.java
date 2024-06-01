19673d50eb9a2f4cfa445f0f457c7ddef36b301b
Jun Kim <82041804+Kim-HJ1986@users.noreply.github.com>
2024-06-01 11:48:49 +0900

    키 만드는 기능 메서드로 분리 (공통화)

--- main-server/src/main/java/com/project/watermelon/repository/ReservationRedisRepository.java
+++ main-server/src/main/java/com/project/watermelon/repository/ReservationRedisRepository.java
@@ -14,10 +14,15 @@ import java.util.concurrent.TimeUnit;
 public class ReservationRedisRepository {
     private final StringRedisTemplate stringRedisTemplate;
 
+    // 공통 키 생성 메서드
+    private String createHashKeyForMemberStatus(String concertMappingId) {
+        return "concertMappingId:" + concertMappingId + ":memberStatus";
+    }
+
     public void storeUserIdWithDefaultState(String memberEmail, String concertMappingId) {
         HashOperations<String, String, String> hashOps = stringRedisTemplate.opsForHash();
 
-        String hashKey = "concertMappingId:" + concertMappingId + ":memberStatus";
+        String hashKey = createHashKeyForMemberStatus(concertMappingId);
 
         // 상태를 Hash에 저장
         hashOps.put(hashKey, memberEmail, ReservationStatus.WAIT.toString());
@@ -25,23 +30,4 @@ public class ReservationRedisRepository {
         // TTL을 10분으로 설정 (10분 = 600초)
         stringRedisTemplate.expire(hashKey, 10, TimeUnit.MINUTES);
     }
-
-
-    public Long getUserRank(String memberEmail, String concertMappingId) {
-        ZSetOperations<String, String> zSetOps = stringRedisTemplate.opsForZSet();
-        String zSetKey = "concert:" + concertMappingId + ":members";
-
-        return zSetOps.rank(zSetKey, memberEmail);
-    }
-
-    public void deleteUserRank(String userId) {
-        ZSetOperations<String, String> zSetOps = stringRedisTemplate.opsForZSet();
-
-        zSetOps.remove("userRankings", userId);
-
-        // 사용자 ID와 관련된 추가 데이터 삭제 (예: Redis String Key)
-        stringRedisTemplate.opsForValue().getOperations().delete("userId:" + userId);
-    }
-
-
 }
