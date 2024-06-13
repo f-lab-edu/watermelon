4aada5a222e838a0e7039e643a7f32e718308398
Jun Kim <82041804+Kim-HJ1986@users.noreply.github.com>
2024-06-14 06:49:14 +0900

    reservation repository named query 로 통일

--- common/src/main/java/com/project/watermelon/repository/ReservationRepository.java
+++ common/src/main/java/com/project/watermelon/repository/ReservationRepository.java
@@ -5,16 +5,19 @@ import com.project.watermelon.enumeration.ReservationStatus;
 import com.project.watermelon.model.Reservation;
 import com.project.watermelon.vo.ConcertMappingSeatInfoVO;
 import com.project.watermelon.vo.ReservationSeatVo;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
     Optional<Reservation> findByMember_Email(String email);
 
     @Query("""
@@ -22,26 +25,18 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
         FROM Reservation R
         JOIN R.concertMapping C
         JOIN C.location L
-        WHERE C.concertDate > CURRENT_TIMESTAMP
+        WHERE C.concertDate > :currentTimestamp
     """)
-    List<ConcertMappingSeatInfoVO> retrieveConcertMappingSeatCapacities();
-    // CURRENT_TIMESTAMP -> 어플리케이션에서 넣어주기
-    // DISTINCT -> 여러개 걸리면 조합으로 걸림 -> DISTINCT 필요 없을 것 같음.
-    // Named query
+    List<ConcertMappingSeatInfoVO> retrieveConcertMappingSeatCapacities(@Param("currentTimestamp") LocalDateTime currentTimestamp);
 
-    @Transactional
-    @Modifying
-    @Query(value = """
-        UPDATE RESERVATION R
-        JOIN CONCERT_MAPPING CM
-        ON R.CONCERT_MAPPING_ID = CM.CONCERT_MAPPING_ID
-        AND CM.CONCERT_DATE > NOW()
-        SET R.STATUS = 'EXPIRED'
-        WHERE R.STATUS = 'AVAILABLE'
-        AND R.AVAILABLE_AT < NOW() - INTERVAL 10 MINUTE
-    """, nativeQuery = true)
-    void updateToExpiredStatus();
-    // now 를 어플리케이션에서 넣어서 넣어주기 -> 데이터베이스 내장 함수 사용 시 인덱스를 먹지 않음!
+    @Query("""
+        SELECT r.reservationId
+        FROM Reservation r
+        WHERE r.concertMapping.concertMappingId = :concertMappingId
+        AND r.status = 'WAIT'
+        ORDER BY r.reservationRank
+    """)
+    List<Long> findReservationIdsForUpdate(@Param("concertMappingId") Long concertMappingId, Pageable pageable);
 
     @Query("""
         SELECT COUNT(*)
@@ -51,41 +46,38 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
     """)
     Long retrieveAvailableOrReservedCount(@Param("concertMappingId") Long concertMappingId, @Param("statuses") List<ReservationStatus> statuses);
 
-
-    @Transactional
-    @Modifying
-    @Query(value = """
-        UPDATE RESERVATION R
-        JOIN (
-            SELECT RESERVATION_ID
-            FROM RESERVATION
-            WHERE CONCERT_MAPPING_ID = :concertMappingId AND STATUS = 'WAIT'
-            ORDER BY RESERVATION_RANK
-            LIMIT :count
-        ) subquery
-        ON R.RESERVATION_ID = subquery.RESERVATION_ID
-        SET
-            R.STATUS = 'AVAILABLE',
-            R.AVAILABLE_AT = NOW()
-    """, nativeQuery = true)
-    void updateReservationStatus(@Param("concertMappingId") Long concertMappingId, @Param("count") Long count);
-    // NOW() 빼고 어플리케이션 레벨에서 넣어주기.
-    // JOIN -> 종류 별 성능 다른점
-    // 셀프 조인을 쓰는 방식은 일반적이지 않아서 -> 제너럴한 쿼리 공부해보기! <- 로직 자체는 괜찮음!
-    // JPA 로 사용하는게 훨씬 깔끔할 것으로 보임!
-    // 아래 두 어노테이션 사용하는 이유에 대해서 공부!
-    // @Transactional
-    // @Modifying
-
     @Query("""
-    SELECT new com.project.watermelon.vo.ReservationSeatVo(R.reservationId, T.ticketId, T.seat.seatId)
-    FROM Reservation R
-    JOIN Ticket T
-    ON R.ticketId = T.ticketId
-    WHERE R.concertMapping.concertMappingId = :concertMappingId
-    AND R.status IN :statuses
+        SELECT new com.project.watermelon.vo.ReservationSeatVo(R.reservationId, T.ticketId, T.seat.seatId)
+        FROM Reservation R
+        JOIN Ticket T
+        ON R.ticketId = T.ticketId
+        WHERE R.concertMapping.concertMappingId = :concertMappingId
+        AND R.status IN :statuses
     """)
-    List<ReservationSeatVo> findByConcertMappingIdAndStatuses(@Param("concertMappingId") Long concertMappingId,
-                                                     @Param("statuses") List<ReservationStatus> statuses);
+    List<ReservationSeatVo> findByConcertMappingIdAndStatuses(@Param("concertMappingId") Long concertMappingId, @Param("statuses") List<ReservationStatus> statuses);
 
+    @Modifying
+    @Transactional
+    @Query("""
+        UPDATE Reservation r
+        SET r.status = 'EXPIRED'
+        WHERE r.status = 'AVAILABLE'
+        AND r.availableAt < :expiryTime
+        AND r.concertMapping.concertMappingId IN (
+            SELECT cm.concertMappingId
+            FROM ConcertMapping cm
+            WHERE cm.concertDate > :currentTimestamp
+        )
+    """)
+    void updateToExpiredStatus(@Param("currentTimestamp") LocalDateTime currentTimestamp, @Param("expiryTime") LocalDateTime expiryTime);
+
+    @Modifying
+    @Transactional
+    @Query("""
+        UPDATE Reservation r
+        SET r.status = 'AVAILABLE', r.availableAt = :currentTimestamp
+        WHERE r.reservationId IN :reservationIdList
+    """)
+    void updateReservationStatus(@Param("reservationIdList") List<Long> reservationIdList, @Param("currentTimestamp") LocalDateTime currentTimestamp);
 }
+
--- consumer-server/src/main/java/com/project/consumerserver/service/ReservationCronJobService.java
+++ consumer-server/src/main/java/com/project/consumerserver/service/ReservationCronJobService.java
@@ -4,9 +4,12 @@ import com.project.watermelon.enumeration.ReservationStatus;
 import com.project.watermelon.repository.ReservationRepository;
 import com.project.watermelon.vo.ConcertMappingSeatInfoVO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
@@ -21,10 +24,12 @@ public class ReservationCronJobService {
         // 전체 전제 조건: 공연일이 현재 보다 미래인 데이터에 대해서만 수행.
         // 예매 테이블에서 AVAILABLE_AT -> 10분 지난 경우 일괄 EXPIRED 로 update
         // 결제 시 reservation lock check -> 분산락 키워드 검색해보기!
-        reservationRepository.updateToExpiredStatus();
+        LocalDateTime currentTimestamp = LocalDateTime.now();
+        LocalDateTime expiryTime = currentTimestamp.minusMinutes(10);
+        reservationRepository.updateToExpiredStatus(currentTimestamp, expiryTime);
 
         // 예매 테이블에서 유니크한 CONCERT_MAPPING_ID 조회 (단 공연일이 지난 것들은 제외)
-        List<ConcertMappingSeatInfoVO> concertMappingSeatInfoList = reservationRepository.retrieveConcertMappingSeatCapacities();
+        List<ConcertMappingSeatInfoVO> concertMappingSeatInfoList = reservationRepository.retrieveConcertMappingSeatCapacities(currentTimestamp);
 
         // 해당 CONCERT_MAPPING_ID 별 예매 상태 업데이트 플로우 시작
         // 업데이트 가능한 수 = (LOCATION 의 SEAT_CAPACITY - CONCERT_MAPPING_ID 기준 AVAILABLE, RESERVED 상태인 로우 카운트)
@@ -34,9 +39,17 @@ public class ReservationCronJobService {
             List<ReservationStatus> statuses = Arrays.asList(ReservationStatus.AVAILABLE, ReservationStatus.RESERVED);
 
             Long availableOrReservedCount = reservationRepository.retrieveAvailableOrReservedCount(targetConcertMappingId, statuses);
-            Long updateCount = (targetSeatCapacity - availableOrReservedCount);
+            long updateCount = (targetSeatCapacity - availableOrReservedCount);
+            int updateCountInt = Math.toIntExact(updateCount);
 
-            reservationRepository.updateReservationStatus(targetConcertMappingId, updateCount);
+            // 1. 예약 ID를 페이징을 사용하여 가져옴
+            Pageable pageable = PageRequest.of(0, updateCountInt);
+            List<Long> reservationIdList = reservationRepository.findReservationIdsForUpdate(targetConcertMappingId, pageable);
+
+            // 2. 가져온 예약 ID를 사용하여 상태 업데이트
+            if (!reservationIdList.isEmpty()) {
+                reservationRepository.updateReservationStatus(reservationIdList, currentTimestamp);
+            }
         }
     }
 }
