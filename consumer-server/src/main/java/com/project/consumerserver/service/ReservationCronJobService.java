2cd28bb94556a54eb1dadd247031e9395916cb5f
Jun Kim <82041804+Kim-HJ1986@users.noreply.github.com>
2024-06-23 21:29:11 +0900

    예매 크론잡 로직 리팩토링 (메서드 분리 및 트랜잭션 조정)

--- consumer-server/src/main/java/com/project/consumerserver/service/ReservationCronJobService.java
+++ consumer-server/src/main/java/com/project/consumerserver/service/ReservationCronJobService.java
@@ -6,7 +6,6 @@ import com.project.watermelon.model.Reservation;
 import com.project.watermelon.repository.ConcertMappingRepository;
 import com.project.watermelon.repository.ReservationRepository;
 import com.project.watermelon.vo.ConcertMappingSeatInfoVO;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -22,68 +21,59 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class ReservationCronJobService {
 
+    private static final String LOCKED_RESERVATION_LIST_KEY = "lockedReservationList";
+    private static final int PAGE_SIZE = 1000;
+    private static final int EXPIRY_MINUTES = 10;
+
     private final StringRedisTemplate stringRedisTemplate;
     private final ReservationRepository reservationRepository;
     private final ConcertMappingRepository concertMappingRepository;
+    private final ReservationService reservationService;
 
     @Scheduled(cron = "0 */1 * * * *")
-    @Transactional
     public void updateReservation() {
-        // 전체 전제 조건: 공연일이 현재 보다 미래인 데이터에 대해서만 수행.
-        // 예매 테이블에서 AVAILABLE_AT -> 10분 지난 경우 일괄 EXPIRED 로 update
-        // 결제 시 reservation lock check -> 분산락 키워드 검색해보기!
+        // expired 기준 시점 할당
         LocalDateTime currentTimestamp = LocalDateTime.now();
-        LocalDateTime expiryTime = currentTimestamp.minusMinutes(10);
+        LocalDateTime expiryTime = currentTimestamp.minusMinutes(EXPIRY_MINUTES);
 
-        // 락이 걸린 예약 ID 리스트 조회
-        String lockedReservationListKey = "lockedReservationList";
-        Set<String> lockedReservationIdSet = Optional.ofNullable(stringRedisTemplate.opsForSet().members(lockedReservationListKey)).orElse(Collections.emptySet());
-        Set<Long> lockedReservationIds = lockedReservationIdSet.stream().map(Long::valueOf).collect(Collectors.toSet());
+        // reservation lock 이 걸려있는 Reservation 목록 id 조회
+        Set<Long> lockedReservationIds = getLockedReservationIds();
+        // 아직 실행되지 않은 ConcertMapping 목록 조회
+        List<Long> concertMappingIds = getFutureConcertMappingIds(currentTimestamp);
 
-        // 만료될 예약 데이터 조회
-        List<Long> concertMappingIds = concertMappingRepository.findByConcertDateAfter(currentTimestamp).stream()
+        // reservation lock 이 걸려있지 않은 Reservation 중 조건 충족하는 객체 상태 EXPIRED 로 업데이트
+        List<Reservation> filteredReservationsToExpire = getFilteredReservationsToExpire(expiryTime, concertMappingIds, lockedReservationIds);
+        reservationService.updateReservationsToExpired(filteredReservationsToExpire);
+
+        List<ConcertMappingSeatInfoVO> concertMappingSeatInfoList = getUniqueConcertMappingSeatInfo(currentTimestamp);
+        reservationService.updateReservationStatus(concertMappingSeatInfoList, currentTimestamp);
+    }
+
+    private Set<Long> getLockedReservationIds() {
+        Set<String> lockedReservationIdSet = Optional.ofNullable(stringRedisTemplate.opsForSet().members(LOCKED_RESERVATION_LIST_KEY)).orElse(Collections.emptySet());
+        return lockedReservationIdSet.stream().map(Long::valueOf).collect(Collectors.toSet());
+    }
+
+    private List<Long> getFutureConcertMappingIds(LocalDateTime currentTimestamp) {
+        return concertMappingRepository.findByConcertDateAfter(currentTimestamp).stream()
                 .map(ConcertMapping::getConcertMappingId)
                 .collect(Collectors.toList());
+    }
 
-        // pageable 을 통해 1000개 씩 조회
-        Pageable expiredPageable = PageRequest.of(0, 1000);
+    private List<Reservation> getFilteredReservationsToExpire(LocalDateTime expiryTime, List<Long> concertMappingIds, Set<Long> lockedReservationIds) {
+        Pageable expiredPageable = PageRequest.of(0, PAGE_SIZE);
         List<Reservation> reservationToExpire = reservationRepository.findByStatusAndAvailableAtBeforeAndConcertMappingConcertMappingIdIn(
                 ReservationStatus.AVAILABLE, expiryTime, concertMappingIds, expiredPageable);
-        // lockedReservationIds 에 해당하는 예약 데이터 제거
-        List<Reservation> filteredReservationsToExpire = reservationToExpire.stream()
+        return reservationToExpire.stream()
                 .filter(reservation -> !lockedReservationIds.contains(reservation.getReservationId()))
                 .toList();
+    }
 
-        // 예약 EXPIRED 상태로 업데이트
-        filteredReservationsToExpire.forEach(Reservation::updateReservationStatusExpire);
-        reservationRepository.saveAll(filteredReservationsToExpire);
-
-        // 예매 테이블에서 유니크한 CONCERT_MAPPING_ID 조회 (단 공연일이 지난 것들은 제외)
+    private List<ConcertMappingSeatInfoVO> getUniqueConcertMappingSeatInfo(LocalDateTime currentTimestamp) {
         List<Reservation> reservations = reservationRepository.findDistinctByConcertMappingConcertDateAfter(currentTimestamp);
-        List<ConcertMappingSeatInfoVO> concertMappingSeatInfoList = reservations.stream()
-        .map(r -> new ConcertMappingSeatInfoVO(r.getConcertMapping().getConcertMappingId(), r.getConcertMapping().getLocation().getSeatCapacity()))
-        .distinct()
-        .toList();
-
-        // 해당 CONCERT_MAPPING_ID 별 예매 상태 업데이트 플로우 시작
-        // 업데이트 가능한 수 = (LOCATION 의 SEAT_CAPACITY - CONCERT_MAPPING_ID 기준 AVAILABLE, RESERVED 상태인 로우 카운트)
-        for (ConcertMappingSeatInfoVO concertMappingSeatInfo : concertMappingSeatInfoList) {
-            Long targetConcertMappingId = concertMappingSeatInfo.getConcertMappingId();
-            Long targetSeatCapacity = concertMappingSeatInfo.getSeatCapacity();
-            List<ReservationStatus> statuses = Arrays.asList(ReservationStatus.AVAILABLE, ReservationStatus.RESERVED);
-
-            Long availableOrReservedCount = reservationRepository.countByStatusInAndConcertMappingConcertMappingId(
-                    statuses, targetConcertMappingId);
-            long updateCount = (targetSeatCapacity - availableOrReservedCount);
-            int updateCountInt = Math.toIntExact(updateCount);
-
-            // 예약 데이터를 페이징을 사용하여 조회
-            Pageable availablePageable = PageRequest.of(0, updateCountInt);
-            List<Reservation> reservationsToAvailable = reservationRepository.findByConcertMappingConcertMappingIdAndStatusOrderByReservationRank(
-                    targetConcertMappingId, ReservationStatus.WAIT, availablePageable);
-            // AVAILABLE status 로 업데이트
-            reservationsToAvailable.forEach(reservation -> reservation.updateReservationStatusAvailable(currentTimestamp));
-            reservationRepository.saveAll(reservationsToAvailable);
-        }
+        return reservations.stream()
+                .map(r -> new ConcertMappingSeatInfoVO(r.getConcertMapping().getConcertMappingId(), r.getConcertMapping().getLocation().getSeatCapacity()))
+                .distinct()
+                .toList();
     }
 }
