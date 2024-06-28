d8f69b04e367cfb32b251e6f00dd9fdf31d7406a
Jun Kim <82041804+Kim-HJ1986@users.noreply.github.com>
2024-06-29 00:38:25 +0900

    redis lock aspect refactoring

--- main-server/src/main/java/com/project/watermelon/aspect/RedisLockAspect.java
+++ main-server/src/main/java/com/project/watermelon/aspect/RedisLockAspect.java
@@ -14,12 +14,13 @@ import org.springframework.stereotype.Component;
 public class RedisLockAspect {
 
     private final RedisLockService redisLockService;
+    private static final int REDIS_TTL_SECONDS = 300;
 
     @Around("@annotation(com.project.watermelon.annotation.RedisLock) && args(reservationId,..)")
     public Object aroundProcessPayment(ProceedingJoinPoint joinPoint, Long reservationId) throws Throwable {
-        String lockKey = "reservationLock:" + reservationId;
+        String lockKey = getLockKey(reservationId);
         try {
-            boolean locked = redisLockService.lock(lockKey, 300);
+            boolean locked = redisLockService.lock(lockKey, REDIS_TTL_SECONDS);
             if (!locked) {
                 throw new RedisLockException("Failed to acquire lock for reservation: " + reservationId);
             }
@@ -29,5 +30,10 @@ public class RedisLockAspect {
         } finally {
             redisLockService.unlock(lockKey);
         }
+    }
+
+    private String getLockKey(Long reservationId) {
+        // 추가적인 더하기 연산이 발생할 경우 StringBuilder 를 사용하여 메모리 효율성 개선
+        return "reservationLock:" + reservationId;
     }
 }
