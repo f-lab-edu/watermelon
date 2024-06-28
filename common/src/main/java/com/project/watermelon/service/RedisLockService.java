c526d69b6d6efb4e50dee42bd62ecf094355ad6c
Jun Kim <82041804+Kim-HJ1986@users.noreply.github.com>
2024-06-29 00:37:49 +0900

    분산락 획득 로직 마무리

--- common/src/main/java/com/project/watermelon/service/RedisLockService.java
+++ common/src/main/java/com/project/watermelon/service/RedisLockService.java
@@ -13,28 +13,17 @@ public class RedisLockService {
     private final StringRedisTemplate stringRedisTemplate;
 
     public boolean lock(String key, long ttlInSeconds) {
-        // 메인 서버의 동시성 이슈 및 중복 처리 방지를 위해 SETNX (setIfAbsent) 사용
+        // 동시성 이슈 및 중복 처리 방지를 위해 SETNX (setIfAbsent) 사용
         Boolean success = stringRedisTemplate.opsForValue().setIfAbsent(
                 key, "locked",
                 ttlInSeconds,
                 TimeUnit.SECONDS
         );
-        if (Boolean.TRUE.equals(success)) {
-            addLockedReservationId("lockedReservationList", key);
-        }
         return Boolean.TRUE.equals(success);
     }
 
     public void unlock(String key) {
         stringRedisTemplate.delete(key);
-        removeLockedReservationId("lockedReservationList", key);
     }
 
-    private void addLockedReservationId(String setKey, String reservationId) {
-        stringRedisTemplate.opsForSet().add(setKey, reservationId);
-    }
-
-    private void removeLockedReservationId(String setKey, String reservationId) {
-        stringRedisTemplate.opsForSet().remove(setKey, reservationId);
-    }
 }
