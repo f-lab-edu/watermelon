a6f6568c832f2fa0fe921fa8104395aecd05748d
Jun Kim <82041804+Kim-HJ1986@users.noreply.github.com>
2024-06-29 00:38:38 +0900

    component scan scope 수정

--- consumer-server/src/main/java/com/project/consumerserver/ConsumerServerApplication.java
+++ consumer-server/src/main/java/com/project/consumerserver/ConsumerServerApplication.java
@@ -2,12 +2,14 @@ package com.project.consumerserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.project.watermelon"})
 public class ConsumerServerApplication {
 
 	public static void main(String[] args) {
